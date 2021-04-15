6e24fd1c25824be8323a9ee2eede9a01d8398630
AndreyZvoznikov <zvznkv.andrey@gmail.com>
2021-04-15 15:10:50 +0300

    pdf+img+mailer

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,33 +1,35 @@
-"use strict";
-const nodemailer = require("nodemailer");
+'use strict';
+const nodemailer = require('nodemailer');
 
 async function main() {
   // let testAccount = await nodemailer.createTestAccount('smtps://USERNAME%40gmail.com:PASSWORD@smtp.gmail.com');
 
   let transporter = nodemailer.createTransport({
-    host: "smtp.yandex.ru",
+    host: 'smtp.yandex.ru',
     port: 465,
-    secure: true, 
+    secure: true,
     auth: {
       user: 'shopper-store@yandex.ru',
       pass: 'passwordforshopperstore',
     },
   });
 
-
   let info = await transporter.sendMail({
-    from: 'shopper-store@yandex.ru', 
-    to: "zvoznikovandrey@gmail.com",
-    subject: "Спецификация",
-    text: "Добрый день. Во вложении спецификация",
-    attachments: {
-      path: './pdf/pdfs/Спецификация.pdf'
-    }
+    from: 'shopper-store@yandex.ru',
+    to: 'zvoznikovandrey@gmail.com',
+    subject: 'Спецификация',
+    text: 'Добрый день. Во вложении спецификация',
+    attachments: [
+      {
+        path: './pdf/pdfs/Спецификация.pdf',
+      },
+      { path: `./public/img/img.jpeg` },
+    ],
     // html: "<b>Hello world?</b>",
   });
 
-  console.log("Message sent: %s", info.messageId);
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  console.log('Message sent: %s', info.messageId);
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 }
 
 main().catch(console.error);
