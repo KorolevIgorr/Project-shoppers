f63e38ad3abe1c3dc4d6d402d209c166a31065ca
Igor <korol_all@mail.ru>
2021-04-15 15:29:58 +0300

    hbs orderFormOne and header and style

--- routes/order.js
+++ routes/order.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.render('orderFormOne'); // ПОМЕНЯЙ НАЗВАНИЕ, КАК И ИГОРЯ!!!
+  res.render('orderFormOne'); // ПОМЕНЯЙ НАЗВАНИЕ, КАК У ИГОРЯ!!!
 });
 
 module.exports = router;
